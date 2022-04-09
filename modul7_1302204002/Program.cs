670e2aa1d976f2b7f126b75d357dd2ec70afcce4
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-04-09 15:40:52 +0700

    Add project files.

---
+++ modul7_1302204002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul7_1302204002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
