f4352fc2e880e5777beca66e91cab42b28e9f3ab
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-04-09 17:17:12 +0700

    “aplikasi bank transfer dengan runtime configuration

---
+++ modul7_1302204002/BankTransferConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul7_1302204002
+{
+    internal class BankTransferConfig
+    {
+        public class Bank
+        {
+            public string lang { get; set; }
+            public transferClass transfer { get; set; }
+            public string methods { get; set; }
+            public confirmationClass confirmation { get; set; }
+        }
+
+        public class transferClass
+        {
+            public string thresold { get; set; }
+            public string low_fee { get; set; }
+            public string high_fee { get; set; }
+        }
+
+        public class confirmationClass
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+
+        public dynamic ReadJson()
+        {
+            string file = "file.json";
+            string data = File.ReadAllText(file);
+
+            Bank bank = JsonSerializer.Deserialize<Bank>(data);
+
+            return bank;
+        }
+
+    }
+}
--- modul7_1302204002/Program.cs
+++ modul7_1302204002/Program.cs
@@ -6,7 +6,93 @@ namespace modul7_1302204002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankTransferConfig btc = new BankTransferConfig();
+
+            dynamic config = GetConf(btc);
+            string konfirmasi;
+            int biaya;
+            int x = 0;
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine(" Please insert the amount of money to transfer : ");
+            }
+            else if (config.lang == "id")
+            {
+                Console.WriteLine(" Masukkan jumlah uang yang akan di-transfer : ");
+            }
+
+            string TransferringMoney = Console.ReadLine();
+            
+            int MoneyinTransfer = int.Parse(TransferringMoney);
+            if (MoneyinTransfer <= (int)config.transfer.threshold)
+            {
+                biaya = config.transfer.low_fee;
+            }
+            else
+            {
+                biaya = config.transfer.high_fee;
+            }
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + biaya);
+                Console.WriteLine("Total amount = " + (biaya + MoneyinTransfer));
+                Console.WriteLine("Select transfer method");
+            }
+            else if (config.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer = " + biaya);
+                Console.WriteLine("Total biaya = " + (biaya + MoneyinTransfer));
+                Console.WriteLine("Pilih metode transfer");
+            }
+
+            
+            foreach (var mthd in config.methods)
+            {
+                x++;
+                Console.WriteLine(x + ". " + mthd);
+            }
+
+            Console.WriteLine();
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Please type '" + config.confirmation.en + " ' to confirm the transaction:");
+
+                konfirmasi = Console.ReadLine();
+
+                if (konfirmasi == (string)config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else if (config.lang == "id")
+            {
+
+                Console.WriteLine("Ketik '" + config.confirmation.id + "' untuk mengkonfirmasi transaksi:");
+
+                konfirmasi = Console.ReadLine();
+
+                if (konfirmasi == (string)config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+        }
+
+        private static dynamic GetConf(BankTransferConfig bankConfig)
+        {
+            return bankConfig.ReadJson();
         }
     }
 }
